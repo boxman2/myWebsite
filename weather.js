12bc4da69ec43e58eb27e4a06702c28bef6fdaa0
hyun <boxman2@naver.com>
2022-09-20 21:06:04 +0900

    Before css

---
+++ weather.js
@@ -0,0 +1,18 @@
+const WEATHER_API_KEY = "bd63b0df1e739e61ce1cbc327828e580"
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&units=metric`
+    fetch(url).then((response) =>response.json()).then((data)=> {
+        const weather = document.querySelector("#weather span:first-child")
+        const city = document.querySelector("#weather span:last-child")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main}/ ${data.main.temp}`
+    })
+}
+function onGeoFail(){
+    alert("Can't find where you live in")
+};
+
+navigator.geolocation.getCurrentPosition(onGeoOk , onGeoFail)
